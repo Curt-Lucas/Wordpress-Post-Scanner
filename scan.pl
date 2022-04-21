444f4ce1269514714f495ee1f06ec8410fca4480
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-04-21 18:17:59 -0400

    Update scan.pl
    add http request object

--- scan.pl
+++ scan.pl
@@ -1,11 +1,15 @@
 use strict;
 use warnings;
+require HTTP::Request;
 
 my $file = 'urls.txt';
 open my $info, $file or die "Could not open $file: $!";
 
 while( my $line = <$info>)  {   
-    print $line;    
+    print $line;
+	
+	$request = HTTP::Request->new(GET => "http://${line}/?s=");
+	
     last if $. == 2;
 }
 
